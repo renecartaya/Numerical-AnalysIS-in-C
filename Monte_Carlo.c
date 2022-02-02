5d12cf54daa0a50de31da1f2c50f3d6fe72e7985
René Cartaya <52467158+renecartaya@users.noreply.github.com>
2022-02-02 23:41:50 +0100

    Add files via upload

---
+++ Monte_Carlo.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int i, N, *array; 
+double a, b, c, d, try, num, sum;
+
+FILE *file;
+
+double fx(double x);
+double fxy(double x, double y);
+
+main()
+{
+  printf("Resolucion de Integrales en 1D y 2D mediante Método Montecarlo\n\n"); 
+
+  printf("Introduzca el número de iteraciones que desea ejecutar\n");
+  scanf("%lf",&N);
+
+
+  printf("Introduzca la cota superior de integración\n");
+  scanf("%lf",&a);
+
+
+  printf("Introduzca la cota inferior de integración\n");
+  scanf("%lf",&b);
+
+
+  printf("\n");
+  scanf("\n");
+
+
+................
+
+    double fx(double x)
+    {
+      double function;
+    }
+
